a2222a02fdb376d39f674cd34684f26b06c939ca
TheGreatKamus <emiliofloresga@gmail.com>
2025-10-17 23:07:54 -0500

    pruebas de creacion de archivo

--- taller-grupal/src/main/java/business/FileManager.java
+++ taller-grupal/src/main/java/business/FileManager.java
@@ -1,14 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package business;
 
-/**
- *
- * @author MikelMZ : Miguel Armas
- */
+import domain.Person;
+import enums.Status;
+import enums.Type;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileManager {
-    //TODO
+
+    private String name;
+    private String lastName;
+    private String identityCard;
+    private String telephone;
+
+    private Status status;
+    private Type type;
+    private String comment = "Hola";
+
+    public void send_history () {
+        File path = new File("src\\main\\java\\history");
+        File history = new File(path, "history.txt");
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(history))) {
+
+            Person person = new Person(name,lastName,identityCard,telephone,status,type);
+            bw.write(person.toString());
+            bw.write(comment);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String setCommnet (){
+        return comment;
+    }
+
+    public static void main (String[] args) {
+        FileManager fm = new FileManager();
+        fm.send_history();
+    }
+
 }
