6d9159bcc10157077a6cbffc19f4ca09b00ada46
M16artz <marmasordonez@gmail.com>
2025-10-20 10:43:40 -0500

    clase bussiness

--- taller-grupal/src/main/java/business/Business.java
+++ taller-grupal/src/main/java/business/Business.java
@@ -1,4 +1,35 @@
 package business;
 
+import domain.Ticket;
+
 public class Business {
+    private TicketsQueue queue;
+    private ChangesStack changesHistorial;
+    
+    public void addToQueue(Ticket ticket){
+        queue.enqueue(ticket);
+    }
+    
+    public Ticket  processTicked(){
+        Ticket actualTicket= queue.dequeue();
+        changesHistorial=null;
+        registerChange(actualTicket);
+        return actualTicket;
+    }
+    
+    public void registerChange(Ticket ticket){
+        changesHistorial.pushUndo(ticket);
+    }
+    
+    public Ticket undoChanges(Ticket actualTicket){
+        changesHistorial.pushRedo(actualTicket);
+        return changesHistorial.popUndo();
+    }
+    
+    public Ticket redoChanges(Ticket actualTicket){
+        changesHistorial.pushUndo(actualTicket);
+        return changesHistorial.popRedo();
+    }
+    
+    
 }
