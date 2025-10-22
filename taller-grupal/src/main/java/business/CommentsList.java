5f3117e7f06ce7e8b365e59329ac1da6b26da227
M16artz <marmasordonez@gmail.com>
2025-10-22 15:52:32 -0500

    Suprimir metodos duplicados

--- taller-grupal/src/main/java/business/CommentsList.java
+++ taller-grupal/src/main/java/business/CommentsList.java
@@ -19,55 +19,6 @@ public class CommentsList {
         return this.head == null;
     }
 
-    public void addFirst(Comments comment) {
-        if (comment == null) {
-            return;
-        }
-
-        Node<Comments> newNode = new Node<>(comment);
-        newNode.setNext(this.head);
-        this.head = newNode;
-    }
-
-
-    public Comments findByDescription(String description) {
-        Node<Comments> current = this.head;
-        while (current != null) {
-            // Compara la descripción del comentario actual con la buscada
-            if (current.getData().getDescription().equals(description)) {
-                return current.getData();
-            }
-            current = current.getNext(); 
-        }
-        return null; // No se encontró
-    }
-
-    public boolean deleteByDescription(String description) {
-        if (isEmpty()) {
-            return false; // No hay nada que borrar
-        }
-
-        if (head.getData().getDescription().equals(description)) {
-            head = head.getNext();
-            return true;
-        }
-
-        Node<Comments> previous = head;
-        Node<Comments> current = head.getNext();
-
-        while (current != null) {
-            if (current.getData().getDescription().equals(description)) {
-                previous.setNext(current.getNext());
-                return true; 
-            }
-     
-            previous = current;
-            current = current.getNext();
-        }
-
-        return false; // No se encontró el elemento
-    }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
